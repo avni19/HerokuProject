2fbe82eb49c51cecdcb578fff88d6dff36ac6610
Avnish Kumar Giri <avnish7870@gmail.com>
2020-08-26 22:41:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def index():
     else:
         return render_template('index.html')
 
-#port = os.getenv("PORT")
+port = os.getenv("PORT")
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
 	#app.run(host = '127.0.0.1', port=8001, debug=True)
